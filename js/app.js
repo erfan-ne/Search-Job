262b53e256f00af8a4c2b08cd7f7b65c50447071
erfan-ne <erfan.neshati@gmail.com>
2025-06-02 19:37:16 +0330

    feat: add javascript codes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,32 @@
 const searchBtn = document.querySelector("#search-btn")
 const wordInput = document.querySelector("#inp-word")
+const resultElem = document.querySelector("#result")
+const sound = document.querySelector("#sound")
 
 const showResult = () => {
-  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${}`)
-}
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput.value}`)
+    .then(response => response.json())
+    .then(data =>{
+      const wordDetails = data[0]
+      resultElem.innerHTML = ""
+
+      console.log(wordDetails);
+      
+      resultElem.innerHTML =
+        `
+        <div class="word">
+          <h3>${wordDetails.word}</h3>
+          <button onclick="playSound()">
+            <i class="fas fa-volume-up"></i>
+          </button>
+        </div>
+        <div class="details">
+          <p>${wordDetails.meanings[0].partOfSpeech}</p>
+          <p>${wordDetails.phonetic}</p>
+        </div>
+        <p class="word-meaning">${wordDetails.meanings[0].definitions[0].definition}</p>
+        `
+    }
+)}
 
 searchBtn.addEventListener("click" , showResult)
